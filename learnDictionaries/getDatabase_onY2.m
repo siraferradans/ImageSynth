b2ae4fc10225ada448d672db6cec1db74c635703
siraferradans <sira.ferradans@gmail.com>
2016-01-22 16:32:31 +0100

    assuming overlapping, we get one every 4 windows

--- learnDictionaries/getDatabase_onY2.m
+++ learnDictionaries/getDatabase_onY2.m
@@ -1,4 +1,4 @@
-function Dl2=getDatabase_onY2(Y2,lambda2,J,Ni)
+function Dl2=getDatabase_onY2(Y2,lambda2)
 %This function saves from Y2 the values (||x conv Psi_j1| conv Psi_j2|), for all
 %j1 (note the two modulus!)
 %Since our scattering can be localized, we obtain the set of corresponding patches of size 2^(J-1) (repatch function).
@@ -16,30 +16,16 @@ j2 = lambda2(1);
 l2 = lambda2(2);
 
 rast = 1;
-%num_patches = Ni/(2^(J-1));
+
+flat=@(x)x(:);
 
 for i=1:length(Y2{3})
     if ((Y2{3}{i}.scale(2) == j2) && (Y2{3}{i}.orientation(2) == l2))
        % Dl2(rast,:,:) = repatch(Y2{3}{i}.asignal,num_patches);
-        Dl2(rast,:) = Y2{3}{i}.asignal(:);
+        Dl2(rast,:) = flat(Y2{3}{i}.asignal(1:2:end,1:2:end)); 
+        %we know there is overlapping between the windows, we get only 1 every 4.
+        
         rast = rast+1;
   %      [Y2{3}{i}.scale(2) Y2{3}{i}.orientation(2) size(Y2{3}{i}.signal)]
     end 
 end
-%Dl2 = reshape(Dl2,[size(Dl2,1)*size(Dl2,2),size(Dl2,3)]);
-
-function v = repatch(signal,num_patches)
-
-N = size(signal);
-w = N/num_patches;%patch size
-
-flat=@(x)x(:);
-X = 1:N/num_patches:(N(1)-w+1);
-Y = 1:N/num_patches:(N(2)-w+1);
-
-for i=0:w-1
-    for j=0:w-1
-        v(i+1,j+1,:)=flat(signal(X(:)+i,Y(:)+j));
-    end 
-end
-v = reshape(v,size(v,1)*size(v,2),size(v,3));
