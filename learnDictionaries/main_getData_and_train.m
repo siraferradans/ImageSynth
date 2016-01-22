72eed604f4002210349345288bc09b68e6c7b2e8
siraferradans <sira.ferradans@gmail.com>
2016-01-22 17:03:57 +0100

    load DB and dict learning in the main, instead of computing the Db

--- learnDictionaries/main_getData_and_train.m
+++ learnDictionaries/main_getData_and_train.m
@@ -1,24 +1,25 @@
 
 
-addpath(genpath('/users/data/ferradan/ImageSynth/code/'));
+% addpath(genpath('/users/data/ferradan/ImageSynth/code/'));
+% 
+% %Obtain statistics on the videos 
+% L=8;
+% 
+% %get Image info
+% disp(['Get filters:'])
+% J = 3;
+% Ni= 128;%2^J;%sqrt(Na*2^(J-1));
+% [filters_image, options_image,lpal] = generate_translate_wavelets([Ni Ni], J, L, 'image');
+% %for reconstruction 
+% 
+% Totalnum_im = 100;
+% path='../../data/' ;
+% 
+% X=getDabase_forDicts(path,Totalnum_im,J,L,Ni,filters_image, options_image);
+% 
+% save('./Database.mat','X','Totalnum_im','J','Ni','L')
 
-%Obtain statistics on the videos 
-L=8;
-
-%get Image info
-disp(['Get filters:'])
-J = 3;
-Ni= 128;%2^J;%sqrt(Na*2^(J-1));
-[filters_image, options_image,lpal] = generate_translate_wavelets([Ni Ni], J, L, 'image');
-%for reconstruction 
-
-Totalnum_im = 100;
-path='../../data/' ;
-
-X=getDabase_forDicts(path,Totalnum_im,J,L,Ni,filters_image, options_image);
-
-save('./Database.mat','X','Totalnum_im','J','Ni','L')
-
+load('./Database.mat');
 disp(['start computing dictionary'])
 %X=D*alpha, we want to get the best dictionary and the alphas for our DB
  for j2=2:J
